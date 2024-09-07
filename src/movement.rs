426c9dfbc8904077d6beaf6c0ac99b24b83f0967
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-09-07 22:36:59 +0200

    dont tryto move to the same spot

--- src/movement.rs
+++ src/movement.rs
@@ -1,33 +1,35 @@
-use crate::action::{handle_action_with_cooldown, AllActionResponse};
-use crate::map::Map;
+use crate::action::{handle_action_with_cooldown, Action, AllActionResponse};
+use crate::character::CharacterData;
+use crate::map::{Map, Position};
 use crate::server::Server;
-use serde_json::{json, Value};
+use serde_json::json;
 
 pub enum Place {
     Bank,
     Resource,
 }
 
-pub async fn move_to(server: &Server, char: &str, place: Place, map: &Map) -> AllActionResponse {
-    let goto = get_pos(place, map);
-    handle_action_with_cooldown(server, crate::action::Action::Move, char, 1, Some(&goto)).await
+pub async fn move_to(server: &Server, char: &CharacterData, place: Place, map: &Map) -> Option<AllActionResponse> {
+    let pos = get_pos(place, map);
+    if char.x == pos.x && char.y == pos.y {
+        return None;
+    }
+
+    let goto = json!({
+        "x": pos.x,
+        "y": pos.y,
+    });
+
+    Some(handle_action_with_cooldown(server, Action::Move, &char.name, 1, Some(&goto)).await)
 }
 
-fn get_pos(place: Place, map: &Map) -> Value {
+fn get_pos(place: Place, map: &Map) -> &Position {
     match place {
         Place::Bank => {
-            let pos = map.bank.iter().next().unwrap().1.iter().next().unwrap();
-            json!({
-            "x": pos.x,
-            "y": pos.y,
-        })
+            map.bank.iter().next().unwrap().1.iter().next().unwrap()
         }
         Place::Resource => {
-            let pos = map.resource.iter().next().unwrap().1.iter().next().unwrap();
-            json!({
-            "x": pos.x,
-            "y": pos.y,
-        })
+            map.resource.iter().next().unwrap().1.iter().next().unwrap()
         }
     }
 }
