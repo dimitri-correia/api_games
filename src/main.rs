f5a668cd15f9aa9418a1280e493a0357d69771bf
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-09-08 00:49:04 +0200

    adapt main

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ mod bank;
 mod movement;
 mod utils;
 mod routines;
+mod monster;
+mod items;
+mod resources;
 
 use crate::character::get_all_chars_infos;
-use crate::map::generate_map;
 use crate::server::create_server;
 use std::error::Error;
 use std::sync::Arc;
@@ -19,14 +21,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let server = Arc::new(create_server());
 
     let characters = get_all_chars_infos(&server).await;
+    let monsters = Arc::new(monster::get_all_monsters(&server).await);
+    let items = Arc::new(items::get_all_items(&server).await);
+    let resources = Arc::new(resources::get_all_resources(&server).await);
+    let map = Arc::new(map::generate_map(&server).await);
 
     let mut handles = Vec::new();
 
     for char in characters {
         let server = Arc::clone(&server);
+        let monsters = Arc::clone(&monsters);
+        let items = Arc::clone(&items);
+        let resources = Arc::clone(&resources);
+        let map = Arc::clone(&map);
 
         let handle = tokio::spawn(async move {
-            routines::action_for_char(char, server).await;
+            routines::action_for_char(char, server, monsters, items, resources, map).await;
         });
 
         handles.push(handle);
