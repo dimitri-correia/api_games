8a3bfb49294abfaa7e2ca0f0d3989b22af39b0a0
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-09-07 21:47:18 +0200

    use character

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ mod routines;
 
 use crate::character::get_all_chars_infos;
 use crate::map::generate_map;
-use crate::routines::action_for_char;
 use crate::server::create_server;
 use std::error::Error;
 use std::sync::Arc;
@@ -21,21 +20,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let map = Arc::new(generate_map(Arc::clone(&server)).await);
 
-    //let characters = vec!["dim", "dim2", "dim3", "dim4", "dim5"];
-    let characters = get_all_chars_infos(&server)
-        .await
-        .iter().map(|(k,_)| k.clone())
-        .collect::<Vec<String>>();
+    let characters = get_all_chars_infos(&server).await;
 
     let mut handles = Vec::new();
 
-    for char in &characters {
+    for char in characters{
         let server = Arc::clone(&server);
+
         let map = Arc::clone(&map);
-        let char = char.to_string();
 
         let handle = tokio::spawn(async move {
-            action_for_char(&char, server, map).await;
+            routines::action_for_char(char, server, map).await;
         });
 
         handles.push(handle);
