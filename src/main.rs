e2477799c8b93203221b9b90bb26318b0a782a5d
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-09-08 00:59:42 +0200

    using struct gameinfo

--- src/main.rs
+++ src/main.rs
@@ -11,32 +11,35 @@ mod monster;
 mod items;
 mod resources;
 
-use crate::character::get_all_chars_infos;
-use crate::server::create_server;
 use std::error::Error;
 use std::sync::Arc;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let server = Arc::new(create_server());
+    let server = Arc::new(server::create_server());
 
-    let characters = get_all_chars_infos(&server).await;
-    let monsters = Arc::new(monster::get_all_monsters(&server).await);
-    let items = Arc::new(items::get_all_items(&server).await);
-    let resources = Arc::new(resources::get_all_resources(&server).await);
-    let map = Arc::new(map::generate_map(&server).await);
+    let characters = character::get_all_chars_infos(&server).await;
+
+    let monsters = monster::get_all_monsters(&server).await;
+    let items = items::get_all_items(&server).await;
+    let resources = resources::get_all_resources(&server).await;
+    let map = map::generate_map(&server).await;
+
+    let game_info = Arc::new(GameInfo {
+        monsters,
+        items,
+        resources,
+        map,
+    });
 
     let mut handles = Vec::new();
 
     for char in characters {
         let server = Arc::clone(&server);
-        let monsters = Arc::clone(&monsters);
-        let items = Arc::clone(&items);
-        let resources = Arc::clone(&resources);
-        let map = Arc::clone(&map);
+        let game_info = Arc::clone(&game_info);
 
         let handle = tokio::spawn(async move {
-            routines::action_for_char(char, server, monsters, items, resources, map).await;
+            routines::action_for_char(char, server, game_info).await;
         });
 
         handles.push(handle);
@@ -50,5 +53,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-
-
+#[derive(Debug, Clone)]
+pub struct GameInfo {
+    pub monsters: Vec<monster::Monster>,
+    pub items: Vec<items::Item>,
+    pub resources: Vec<resources::Resource>,
+    pub map: map::Map,
+}
--- src/routines.rs
+++ src/routines.rs
@@ -1,35 +1,35 @@
 use crate::action::{handle_action_with_cooldown, Action};
-use crate::map::Map;
-use crate::server::Server;
-use crate::utils::handle_cooldown;
-use crate::{bank, character, movement};
-use std::sync::Arc;
 use crate::character::CharacterData;
+use crate::server::Server;
+use crate::GameInfo;
+use std::sync::Arc;
 
-pub async fn action_for_char(character: CharacterData, server_clone: Arc<Server>, map: Arc<Map>) {
-    let map = &*map;
+pub async fn action_for_char(character: CharacterData, server_clone: Arc<Server>, game_info: Arc<GameInfo>) {
+    let mut character = character;
+
     loop {
         // wait cooldown if any
-        let cooldown = character.cooldown as f32;
-        handle_cooldown(&character.name, "cooldown remaining", cooldown).await;
+        // let cooldown_expiration = &character.cooldown_expiration;
+        // handle_cooldown_expiration(&character.name, "cooldown remaining", cooldown_expiration).await;
 
         // move to bank
-        movement::move_to(&server_clone, &character.name, movement::Place::Bank, map).await;
+        // movement::move_to(&server_clone, &character, movement::Place::Bank, map).await;
 
         // deposit all items
-        bank::deposit_all(&server_clone, &character.name).await;
+        // bank::deposit_all(&server_clone, &character).await;
 
         // get the max item the character can hold
-        let max_item = character::get_char_infos(&server_clone, &character.name).await.inventory_max_items;
+        let max_item = character.inventory_max_items;
 
         // move to resource
-        movement::move_to(&server_clone, &character.name, movement::Place::Resource, map).await;
+        // movement::move_to(&server_clone, &character, movement::Place::Resource, map).await;
 
         // gather resource
         if character.name.eq("dim") {
             handle_action_with_cooldown(&server_clone, Action::Fight, &character.name, 300, None).await;
             continue;
         }
-        handle_action_with_cooldown(&server_clone, Action::Gathering, &character.name, 100, None).await;
+        handle_action_with_cooldown(&server_clone, Action::Gathering, &character.name, max_item, None).await;
     }
 }
+
