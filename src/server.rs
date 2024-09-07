b9ad793cdbe2cfa1c357c7b1ed4f250f0e2f3841
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-09-07 19:27:16 +0200

    create impl for server

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,8 @@
 use dotenv::dotenv;
 use reqwest::header::{HeaderMap, HeaderValue, ACCEPT, AUTHORIZATION, CONTENT_TYPE};
-use reqwest::Client;
+use reqwest::{Client, RequestBuilder};
+use serde_json::Value;
+use std::collections::HashMap;
 use std::env;
 
 #[derive(Clone)]
@@ -23,7 +25,31 @@ pub fn create_server() -> Server {
     );
 
     // return client and headers
-    Server { client: Client::new(), headers }
+    Server {
+        client: Client::new(),
+        headers,
+    }
+}
+
+impl Server {
+    pub fn create_request(&self, link: String, json: Option<&Value>, query: Option<HashMap<&str, &str>>)
+        -> RequestBuilder {
+        let url = format!("https://api.artifactsmmo.com/{}", link);
+
+        let mut request = self.client
+            .post(url)
+            .headers(self.headers.clone());
+
+        if let Some(json) = json {
+            request = request.json(json);
+        }
+
+        if let Some(query) = query {
+            request = request.query(&query);
+        }
+
+        request
+    }
 }
 
 fn get_token() -> String {
