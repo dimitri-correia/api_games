32835ecf5b3f679ff4c69245c15e62281cc5f8f9
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-09-07 22:36:22 +0200

    adapt bank to use character infos

--- src/bank.rs
+++ src/bank.rs
@@ -1,8 +1,8 @@
-use crate::action::{handle_action_with_cooldown, AllActionResponse};
+use crate::action::{handle_action_with_cooldown, Action, AllActionResponse};
+use crate::character::CharacterData;
 use crate::server::Server;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
-use crate::character::get_char_infos;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct DepositItem {
@@ -10,18 +10,21 @@ struct DepositItem {
     quantity: u32,
 }
 
-async fn deposit_item(server: &Server, char: &str, item_code: &str, quantity: u32)-> AllActionResponse {
+async fn deposit_item(server: &Server, char: &str, item_code: &str, quantity: u32) -> AllActionResponse {
     let item_data = DepositItem {
         code: item_code.to_string(),
         quantity,
     };
-    handle_action_with_cooldown(server, crate::action::Action::BankDeposit, char, 1, Some(&json!(item_data))).await
+    handle_action_with_cooldown(server, Action::BankDeposit, char, 1, Some(&json!(item_data))).await
 }
 
 
-pub async fn deposit_all(server: &Server, char: &str) {
-    for i in get_char_infos(server, char).await.inventory {
-        println!("Depositing item: {:?}", i);
+pub async fn deposit_all(server: &Server, char: &CharacterData) {
+    for item in char.inventory.iter().clone() {
+        if item.quantity > 0 {
+            println!("Depositing item: {:?}", item);
+            deposit_item(server, &char.name, &item.code, item.quantity).await;
+        }
     }
     println!("Depositing all items");
 }
