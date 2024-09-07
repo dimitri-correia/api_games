9c89167830ebc6991e08c763d21591a2c331f1b3
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-09-07 21:47:48 +0200

    adapt routine to use character infos

--- src/routines.rs
+++ src/routines.rs
@@ -4,31 +4,32 @@ use crate::server::Server;
 use crate::utils::handle_cooldown;
 use crate::{bank, character, movement};
 use std::sync::Arc;
+use crate::character::CharacterData;
 
-pub async fn action_for_char(char: &str, server_clone: Arc<Server>, map: Arc<Map>) {
+pub async fn action_for_char(character: CharacterData, server_clone: Arc<Server>, map: Arc<Map>) {
     let map = &*map;
     loop {
         // wait cooldown if any
-        let cooldown = character::get_char_infos(&server_clone, char).await.cooldown as f32;
-        handle_cooldown(char, "cooldown remaining", cooldown).await;
+        let cooldown = character.cooldown as f32;
+        handle_cooldown(&character.name, "cooldown remaining", cooldown).await;
 
         // move to bank
-        movement::move_to(&server_clone, char, movement::Place::Bank, map).await;
+        movement::move_to(&server_clone, &character.name, movement::Place::Bank, map).await;
 
         // deposit all items
-        bank::deposit_all(&server_clone, char).await;
+        bank::deposit_all(&server_clone, &character.name).await;
 
-        // get the max item the char can hold
-        let max_item = character::get_char_infos(&server_clone, char).await.inventory_max_items;
+        // get the max item the character can hold
+        let max_item = character::get_char_infos(&server_clone, &character.name).await.inventory_max_items;
 
         // move to resource
-        movement::move_to(&server_clone, char, movement::Place::Resource, map).await;
+        movement::move_to(&server_clone, &character.name, movement::Place::Resource, map).await;
 
         // gather resource
-        if char.eq("dim") {
-            handle_action_with_cooldown(&server_clone, Action::Fight, char, 300, None).await;
+        if character.name.eq("dim") {
+            handle_action_with_cooldown(&server_clone, Action::Fight, &character.name, 300, None).await;
             continue;
         }
-        handle_action_with_cooldown(&server_clone, Action::Gathering, char, 100, None).await;
+        handle_action_with_cooldown(&server_clone, Action::Gathering, &character.name, 100, None).await;
     }
 }
