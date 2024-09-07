821e9334fee4f4213dc6d62a98c017d8512df239
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-09-07 23:57:35 +0200

    improve readability

--- src/movement.rs
+++ src/movement.rs
@@ -1,54 +1,62 @@
-use std::collections::HashMap;
 use crate::action::{handle_action_with_cooldown, Action, AllActionResponse};
 use crate::character::CharacterData;
 use crate::map::{Map, Position};
 use crate::server::Server;
 use serde_json::json;
+use std::collections::HashMap;
 
 pub enum Place {
     Bank,
     Resource,
 }
 
-pub async fn move_to(server: &Server, char: &CharacterData, place: Place, map: &Map) -> Option<AllActionResponse> {
-    let pos_char = Position {
-        x: char.x,
-        y: char.y,
+pub async fn move_to(server: &Server, character: &CharacterData, place: Place, map: &Map) -> Option<AllActionResponse> {
+    let current_position = Position {
+        x: character.x,
+        y: character.y,
     };
-    let pos_to_go = get_pos(place, map, pos_char);
-    if char.x == pos_to_go.x && char.y == pos_to_go.y {
+
+    let target_position = get_target_position(place, map, current_position);
+
+    if current_position == target_position {
         return None;
     }
 
-    let goto = json!({
-        "x": pos_to_go.x,
-        "y": pos_to_go.y,
+    let movement_action = json!({
+        "x": target_position.x,
+        "y": target_position.y,
     });
 
-    Some(handle_action_with_cooldown(server, Action::Move, &char.name, 1, Some(&goto)).await)
+    Some(
+        handle_action_with_cooldown(
+            server,
+            Action::Move,
+            &character.name,
+            1,
+            Some(&movement_action),
+        )
+            .await,
+    )
 }
 
-fn get_pos(place: Place, map: &Map, pos_char: Position) -> &Position {
+fn get_target_position(place: Place, map: &Map, current_position: Position) -> Position {
     match place {
-        Place::Bank => {
-            &find_closest_position(&map.bank, pos_char).unwrap()
-        }
-        Place::Resource => {
-            &find_closest_position(&map.resource, pos_char).unwrap()
-        }
+        Place::Bank => find_closest_position(&map.bank, current_position),
+        Place::Resource => find_closest_position(&map.resource, current_position),
     }
+        .expect("Expected at least one valid position")
 }
 
 fn find_closest_position(
-    map: &HashMap<String, Vec<Position>>,
-    pos_char: Position,
+    positions_map: &HashMap<String, Vec<Position>>,
+    current_position: Position,
 ) -> Option<Position> {
-    map.values()
+    positions_map
+        .values()
         .flat_map(|positions| positions.iter())
         .min_by(|pos1, pos2| {
-            pos1
-                .distance(&pos_char)
-                .partial_cmp(&pos2.distance(&pos_char))
+            pos1.distance(&current_position)
+                .partial_cmp(&pos2.distance(&current_position))
                 .unwrap_or(std::cmp::Ordering::Equal)
         })
         .cloned()
