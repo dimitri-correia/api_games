afa7ed90bfe23e2283363be4bc3460bdd84a7b36
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-09-07 18:52:56 +0200

    tmp not working main

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,20 @@ mod map;
 mod char;
 mod bank;
 mod movement;
+mod utils;
 
 use crate::action::{handle_action_with_cooldown, Action};
 use crate::map::{generate_map, Map};
 use crate::server::{create_server, Server};
 use std::error::Error;
 use std::sync::Arc;
+use crate::utils::handle_cooldown;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let server = Arc::new(create_server());
 
-    let map = Arc::new(generate_map(Arc::clone(&server)).await?);
+    let map = Arc::new(generate_map(Arc::clone(&server)).await);
 
     let chars = vec!["dim", "dim2", "dim3", "dim4", "dim5"];
 
@@ -43,9 +45,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
 }
 
 
-async fn action_for_char(char: &str, server_clone: Arc<Server>, map: Arc<Map>) {
+async fn action_for_char(char: &str, server_clone: Arc<Server>, map: Arc<Result<Map, Box<dyn Error>>>) {
     let map = &*map;
     loop {
+        // wait cooldown if any
+        let cooldown = char::get_char_infos(&server_clone, char).await.unwrap().cooldown as f32;
+        handle_cooldown(char, "cooldown remaining", cooldown).await;
+
         // move to bank
         movement::move_to(&server_clone, char, movement::Place::Bank, map).await;
 
@@ -53,7 +59,7 @@ async fn action_for_char(char: &str, server_clone: Arc<Server>, map: Arc<Map>) {
         bank::deposit_all(&server_clone, char).await;
 
         // get the max item the char can hold
-        let max_item = char::get_char_max_items(&server_clone, char).await.unwrap();
+        let max_item = char::get_char_infos(&server_clone, char).await.unwrap().inventory_max_items;
 
         // move to resource
         movement::move_to(&server_clone, char, movement::Place::Resource, map).await;
