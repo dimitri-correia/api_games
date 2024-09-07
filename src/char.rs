b60db5f04055d2aefa83851290fdf36210ee20de
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-09-07 17:08:35 +0200

    get max and all items

--- src/char.rs
+++ src/char.rs
@@ -1,10 +1,25 @@
 use crate::server::Server;
 use reqwest::{Error, Response};
+use serde_json::Value;
 
 pub async fn get_char_max_items(server: &Server, char: &str) -> Result<u32, Error> {
     let infos = get_char_infos(server, char).await?;
-    let char_infos = serde_json::from_str(&infos.text().await?).expect("Failed to parse JSON");
-    char_infos["data"]["inventory_max_items"]["remaining"].as_u32().unwrap()
+    let char_infos: Value = serde_json::from_str(&infos.text().await?).unwrap();
+
+    let max_items = char_infos["data"]["inventory_max_items"]
+        .as_u64().unwrap() as u32;
+
+    Ok(max_items)
+}
+
+pub async fn get_char_all_items(server: &Server, char: &str) -> Result<Vec<Value>, Error> {
+    let infos = get_char_infos(server, char).await?;
+    let char_infos: Value = serde_json::from_str(&infos.text().await?).unwrap();
+
+    let inventory = char_infos["data"]["inventory"]
+        .as_array().unwrap().to_vec();
+
+    Ok(inventory)
 }
 
 async fn get_char_infos(server: &Server, char: &str) -> Result<Response, Error> {
