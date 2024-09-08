a2912702d6ffc09338491c98a71215914e0d2bca
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-09-08 16:52:59 +0200

    improve logs

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,11 @@
 use tokio::time::{self, Duration};
-use chrono::Local;
+use tracing::info;
 
 pub async fn handle_cooldown(char: &str, action: &str, cooldown: f32) {
-    println!(
-        "[{} - {}] Wait for {}: {}s",
-        Local::now().format("%Y-%m-%d %H:%M:%S"),
-        char,
-        action,
-        cooldown
-    );
+    info(char, &format!("[{}] Wait for {}: {}s", char, action, cooldown));
     time::sleep(Duration::from_secs_f32(cooldown)).await;
+}
+
+pub fn info(char_name: &str, log: &str) {
+    info!("[{}] {}", &char_name, &log);
 }
