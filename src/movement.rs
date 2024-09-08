a26e7b710e5439fb92ba1dc0cee71166444e5abb
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-09-08 13:55:06 +0200

    better handling movements

--- src/movement.rs
+++ src/movement.rs
@@ -1,22 +1,40 @@
 use crate::action::{handle_action_with_cooldown, Action, AllActionResponse};
 use crate::character::CharacterData;
-use crate::gameinfo::map::{Map, Position};
+use crate::gameinfo::map::Position;
+use crate::gameinfo::GameInfo;
 use crate::server::Server;
 use serde_json::json;
 use std::collections::HashMap;
+use std::sync::Arc;
 
 pub enum Place {
     Bank,
-    Resource,
+    Resource(GatherType),
 }
 
-pub async fn move_to(server: &Server, character: &CharacterData, place: Place, map: &Map) -> Option<AllActionResponse> {
+pub enum GatherType {
+    Wood,
+    Fish,
+    Mine,
+}
+
+impl GatherType {
+    fn to_string(&self) -> String {
+        match self {
+            GatherType::Wood => "woodcutting".to_string(),
+            GatherType::Fish => "fishing".to_string(),
+            GatherType::Mine => "mining".to_string(),
+        }
+    }
+}
+
+pub async fn move_to(server: &Server, character: &CharacterData, place: Place, game_info: &Arc<GameInfo>) -> Option<AllActionResponse> {
     let current_position = Position {
         x: character.x,
         y: character.y,
     };
 
-    let target_position = get_target_position(place, map, &current_position);
+    let target_position = get_target_position(place, game_info, &current_position, character);
 
     if current_position == target_position {
         return None;
@@ -39,21 +57,41 @@ pub async fn move_to(server: &Server, character: &CharacterData, place: Place, m
     )
 }
 
-fn get_target_position(place: Place, map: &Map, current_position: &Position) -> Position {
+fn get_target_position(place: Place, game_info: &Arc<GameInfo>, current_position: &Position, character: &CharacterData) -> Position {
     match place {
-        Place::Bank => find_closest_position(&map.bank, current_position),
-        Place::Resource => find_closest_position(&map.resource, current_position),
+        Place::Bank => {
+            let vec: Vec<Position> = game_info.map.bank.values().flat_map(|p| p.clone()).collect();
+
+            find_closest_position(&vec, current_position)
+        },
+        Place::Resource(type_resource) => {
+            handle_resource(game_info, current_position, type_resource, character)
+        }
     }
         .expect("Expected at least one valid position")
 }
 
+fn handle_resource(game_info: &Arc<GameInfo>, current_position: &Position, type_resource: GatherType, character: &CharacterData)
+                   -> Option<Position> {
+    let char_lvl = match type_resource {
+        GatherType::Wood => character.woodcutting_level,
+        GatherType::Fish => character.fishing_level,
+        GatherType::Mine => character.mining_level,
+    };
+
+    let resource = game_info.resources
+        .iter()
+        .filter(|&resource| resource.skill.eq(&type_resource.to_string()) && resource.level <= char_lvl)
+        .max_by_key(|&resource| resource.level).expect("No resource found");
+
+    find_closest_position(&game_info.map.resource.get(&resource.code).expect("Resource doesn't exists"), current_position)
+}
+
 fn find_closest_position(
-    positions_map: &HashMap<String, Vec<Position>>,
+    positions_map: &Vec<Position>,
     current_position: &Position,
 ) -> Option<Position> {
-    positions_map
-        .values()
-        .flat_map(|positions| positions.iter())
+    positions_map.iter()
         .min_by(|pos1, pos2| {
             pos1.distance(&current_position)
                 .partial_cmp(&pos2.distance(&current_position))
