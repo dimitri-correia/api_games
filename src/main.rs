4a68d708e510a86699b6e9b788e15b4cd28eda69
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-09-08 16:54:54 +0200

    adding logs

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,27 @@ mod utils;
 mod routines;
 mod events;
 mod gameinfo;
+mod responsecode;
 
-use crate::gameinfo::get_game_info;
 use std::error::Error;
 use std::sync::Arc;
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("setting default subscriber failed");
+
+    info!("Starting the bot");
+
     let server = Arc::new(server::create_server());
 
     let characters = character::get_all_chars_infos(&server).await;
-    let game_info = get_game_info(&server).await;
+    let game_info = gameinfo::get_game_info(&server).await;
 
     let mut handles = Vec::new();
 
@@ -27,7 +37,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let game_info = Arc::clone(&game_info);
 
         let handle = tokio::spawn(async move {
+            let name = char.name.clone();
+            utils::info(&name, "Starting routine");
             routines::action_for_char(char, server, game_info).await;
+            utils::info(&name, "Routine has ended");
         });
 
         handles.push(handle);
